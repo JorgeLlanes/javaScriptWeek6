b7079debc0a1f309737698e30696dec7c4a4d110
JorgeLlanes <102987755+JorgeLlanes@users.noreply.github.com>
2022-05-11 08:29:45 -0500

    New Modi

--- hard.js
+++ hard.js
@@ -1,7 +1,29 @@
-function firstBtn(color) {
-    document.body.style.background = color;
+// function firstBtn(color) {
+//     document.body.style.background = color;
+// }
+
+// function secBtn(color) {
+//     document.body.style.background = color;
+// }
+
+window.updateTheBackground = function(className) {
+    if(className === "bg-red"){
+        document.body.classList.remove("bg-white")
+        document.body.classList.add(className)
+    }else if(className === "bg-white"){
+        document.body.classList.remove("bg-red")
+        document.body.classList.add(className)
+
 }
 
-function secBtn(color) {
-    document.body.style.background = color;
+        
+        
 }
+
+
+
+
+
+    
+   
+    
